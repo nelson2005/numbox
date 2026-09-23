e02f2360c0cf57e63228dbb0b859400d67f95a28
Nelson, Erik <erik_lee_nelson@yahoo.com>
2026-09-23 19:17:51 +0000

    Leave the root logger to the application and log timings at INFO

--- test/stress_work_runner.py
+++ test/stress_work_runner.py
@@ -90,6 +90,7 @@ Execution of do_create_nodes took 0.159s
 Execution of run took 3.779s (0.664s for 1,000 entities)
 ~0.7Gb memory used (about the same for 1,000 entities)
 """
+import logging
 import sys
 
 from numbox.utils.timer import timer
@@ -104,6 +105,7 @@ def do_make_image(w_):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
     num_of_inputs = 1000
     num_of_entities = 10000
     print("multiple run", file=sys.stderr)
